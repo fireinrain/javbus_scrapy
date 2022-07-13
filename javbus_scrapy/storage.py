c87480822e62c8334a82e1ce9abdca0ff24a3eb2
sunrise <575563079@qq.com>
2022-07-14 03:26:22 +0800

    添加存储模块

---
+++ javbus_scrapy/storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Description: sqlite3 操作相关
+@Author  : fireinrain
+@Site    : https://github.com/fireinrain
+@File    : storage.py
+@Software: PyCharm
+@Time    : 2022/7/14 2:31 AM
+"""
+
+if __name__ == '__main__':
+    pass
